c1fb53241f78c845b9887c50961fd5c34392fe68
Jonathan Wapman <jdwapman@ucdavis.edu>
2020-10-02 11:14:47 -0700

    Fix double untar

--- download_all.sh
+++ download_all.sh
@@ -7,7 +7,6 @@ TOTAL_SIZE=0
 for (( i=1; i <= ${NUM_PROBLEMS}; ++i )); do
     # ./ssget -i $i -j
     ARCHIVE=$(./ssget -i $i -f)
-    tar -xvf ${ARCHIVE}
     SIZE=($(du -k ${ARCHIVE}))
     echo "${ARCHIVE}: ${SIZE}KB"
     TOTAL_SIZE=$((${TOTAL_SIZE} + ${SIZE}))
